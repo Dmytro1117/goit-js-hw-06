be36910e77bf6bba34a82a11ae7a513705e1ae7b
Dmytro <111579384+Dmytro1117@users.noreply.github.com>
2022-11-03 14:02:46 +0200

    add4

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,25 @@
-const list = document.querySelectorAll(".item");
-console.log(`Number of categories:`, list.length);
+// const list = document.querySelectorAll(".item");
+// console.log(`Number of categories:`, list.length);
 
-const items = document.querySelectorAll(".item ul");
-const title = document.querySelectorAll("h2");
+// const items = document.querySelectorAll(".item ul");
+// const title = document.querySelectorAll("h2");
+ 
+ 
+// for (let tit of title) { 
+//   console.log(`Category:`,  tit.textContent); 
+// }
 
-console.log(`Category:`, title[0].textContent);
-const Elements1 = items[0].children;
-console.log(`Elements:`, Elements1.length);
+// for (let item of items) { 
+//     console.log(`Elements:`, item.children.length); 
+// }
 
-console.log(`Category:`, title[1].textContent);
-const Elements2 = items[1].children;
-console.log(`Elements:`, Elements2.length);
 
-console.log(`Category:`, title[2].textContent);
-const Elements3 = items[2].children;
-console.log(`Elements:`, Elements3.length);
+
+const list = document.querySelectorAll('.item'); 
+ 
+console.log(`Number of categories:`, list.length); 
+ 
+for (let li of list) { 
+  console.log(`Category: ${li.firstElementChild.textContent}`); 
+  console.log(`Elements: ${li.lastElementChild.children.length}`); 
+}
